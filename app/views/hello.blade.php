5433b9cb49d4497826da32e192799fcb4b65e83a
Gary Saunders <gary@powrit.com>
2013-05-16 23:36:10 -0400

    Added font-awesome assets from L4layouts bundle and things like that.

---
+++ app/views/hello.blade.php
@@ -0,0 +1,9 @@
+@extends('l4layouts::foundation.blank')
+@section('body')
+<div class="row">
+    <div class="large-12 columns">
+        <h1>Hello World!</h1>
+        <p>This is Zurb Foundation 4.1 with Font-Awesome! <i class="icon-star">&nbsp;</i></p>    
+    </div>
+</div>
+@endsection
