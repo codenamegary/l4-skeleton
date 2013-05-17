548f36ae5cd94757332f6965d3465ef1ee679eaa
Gary Saunders <gary@powrit.com>
2013-05-17 00:49:44 -0400

    User migration and pagoda specific config.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,8 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('localhost'),
+	'pagoda' => array('*.pagodabox.com','*.gopagoda.com'),
 
 ));
 
